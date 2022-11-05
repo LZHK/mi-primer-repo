ef6286630b980de69553b0b1ae972d864cd47124
Luis <git@bluuweb.cl>
2022-11-04 18:21:50 -0600

    mi primer commit

---
+++ Codigo.js
@@ -0,0 +1,46 @@
+//Codigo para que funcione el slider
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item,i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+//Codigo para que funcione el slider
+
+//Codigo para validación de datos
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(pass.value.length < 8){
+        warnings += `La contraseña no es valida <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "se ha creado la cuenta"
+    }
+})
+//Codigo para validación de datos
